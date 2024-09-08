477a7586646e9f4a771ca783925d7f06bcc117cf
Berkay Derin <zewodi@gmail.com>
2024-09-09 02:29:13 +0300

    feat: add font

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,13 @@
-import localFont from 'next/font/local'
+import { Roboto } from 'next/font/google'
 import './globals.css'
 import NextAuthSessionProvider from '@/providers/NextAuthSessionProvider'
 import { Toaster } from '@/components/ui/toaster'
 
-const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900'
-})
-const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900'
+const roboto = Roboto({
+	weight: ['300', '400', '500', '700'],
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-roboto'
 })
 
 export const metadata = {
@@ -22,9 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`${roboto.variable} font-sans antialiased`}>
 				<NextAuthSessionProvider>{children}</NextAuthSessionProvider>
 				<Toaster />
 			</body>
