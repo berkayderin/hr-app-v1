384a7a8758b0eb04558bd2f8734b0459a47e1099
Berkay Derin <zewodi@gmail.com>
2024-09-11 17:02:47 +0300

    feat: dark moda

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Roboto } from 'next/font/google'
 import './globals.css'
 import NextAuthSessionProvider from '@/providers/NextAuthSessionProvider'
 import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from '@/components/ModeToggle'
 
 const roboto = Roboto({
 	weight: ['300', '400', '500', '700'],
@@ -19,8 +21,20 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={`${roboto.variable} font-sans antialiased`}>
-				<NextAuthSessionProvider>{children}</NextAuthSessionProvider>
-				<Toaster />
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<div className="fixed top-0 right-0 p-4 z-50">
+						<ModeToggle />
+					</div>
+					<NextAuthSessionProvider>
+						{children}
+					</NextAuthSessionProvider>
+					<Toaster />
+				</ThemeProvider>
 			</body>
 		</html>
 	)
